737ba2f0adb941dbbeb41bf263d95373114cad34
Stephen Toon <satoon101@gmail.com>
2015-08-04 10:37:40 -0400

    Added configuration to allow for flashing the thrower.  This resolves #2.

--- addons/source-python/plugins/anti_team_flash/__init__.py
+++ addons/source-python/plugins/anti_team_flash/__init__.py
@@ -1,3 +1,3 @@
 # ../anti_team_flash/__init__.py
 
-"""."""
+"""Plugin that stops players from being flashed by teammates."""
--- addons/source-python/plugins/anti_team_flash/anti_team_flash.py
+++ addons/source-python/plugins/anti_team_flash/anti_team_flash.py
@@ -1,11 +1,15 @@
 # ../anti_team_flash/anti_team_flash.py
 
-"""."""
+"""Stops players from being flashed by teammates."""
 
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
 # Source.Python Imports
+#   Config
+from config.manager import ConfigManager
+#   Cvars
+from cvars.flags import ConVarFlags
 #   Entities
 from entities.hooks import EntityPostHook
 from entities.hooks import EntityPreHook
@@ -13,8 +17,14 @@ from entities.hooks import EntityPreHook
 from memory import make_object
 #   Players
 from players.entity import PlayerEntity
+from players.helpers import userid_from_index
+#   Translations
+from translations.strings import LangStrings
 #   Weapons
 from weapons.entity import WeaponEntity
+
+# Script Imports
+from anti_team_flash.info import info
 
 
 # =============================================================================
@@ -22,6 +32,23 @@ from weapons.entity import WeaponEntity
 # =============================================================================
 # Create the team variable to check against later
 _flashbang_team = None
+
+# Create the thrower variable to check against later
+_flashbang_thrower = None
+
+# Get the config strings to use
+config_strings = LangStrings(info.basename)
+
+
+# =============================================================================
+# >> CONFIGURATION
+# =============================================================================
+# Create the config file
+with ConfigManager(info.basename, 'atf_') as config:
+
+    # Create the thrower convar
+    flash_thrower = config.cvar(
+        'flash_thrower', 0, ConVarFlags.NONE, config_strings['Thrower'])
 
 
 # =============================================================================
@@ -47,17 +74,29 @@ def _pre_player_deafen(args):
 
 @EntityPreHook('CFlashbangProjectile', 'detonate')
 def _pre_flashbang_detonate(args):
-    """Store the flashbang's team to compare against player teams."""
-    global _flashbang_team
+    """Store the flashbang's thrower/team to compare against player teams."""
+    global _flashbang_team, _flashbang_thrower
+
+    # Get the weapon's instance
     weapon = make_object(WeaponEntity, args[0])
+
+    # Store the weapon's team
     _flashbang_team = weapon.team
+
+    # Get the weapon's thrower
+    owner = weapon.current_owner
+
+    # Store the owner's userid
+    if owner is not None:
+        _flashbang_thrower = userid_from_index(owner.index)
 
 
 @EntityPostHook('CFlashbangProjectile', 'detonate')
 def _post_detonate(args, return_value):
-    """Reset the team variable so that only flashbang blinding is blocked."""
-    global _flashbang_team
+    """Reset the variables so that only flashbang blinding is blocked."""
+    global _flashbang_team, _flashbang_thrower
     _flashbang_team = None
+    _flashbang_thrower = None
 
 
 # =============================================================================
@@ -72,6 +111,13 @@ def _block_team_flash(args):
     # Get the player's instance
     player = make_object(PlayerEntity, args[0])
 
-    # Block the function if the teams match
-    if player.team == _flashbang_team:
-        return False
+    # Is the player on a different team than the thrower?
+    if player.team != _flashbang_team:
+        return
+
+    # Allow self flash
+    if player.userid == _flashbang_thrower and flash_thrower.get_int():
+        return
+
+    # Block the flash for the player
+    return False
