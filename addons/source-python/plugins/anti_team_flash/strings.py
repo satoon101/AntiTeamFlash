86c1b3ee0e06ffb270c091cbe24315d8354e4539
satoon101 <satoon101@gmail.com>
2025-06-19 18:30:24 -0400

    Minor change.

--- addons/source-python/plugins/anti_team_flash/strings.py
+++ addons/source-python/plugins/anti_team_flash/strings.py
@@ -1,6 +1,6 @@
 # ../anti_team_flash/strings.py
 
-"""Contains all translation variables for the base plugin."""
+"""Contains all translation variables for the plugin."""
 
 # =============================================================================
 # >> IMPORTS
